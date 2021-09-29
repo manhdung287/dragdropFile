0522bf2ee7ea6da46fbe572dbfa6fba4f0e451db
manhdung287 <manhdung28797@gmail.com>
2021-09-29 17:01:00 +0700

    add asssets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [hover, setHoverItem] = useState(false);
+  const onChange = (e) => {
+    const { files } = e.target;
+    setData([...data, files[0]])
+  }
+  const setHover = () => {
+    setHoverItem(!hover)
+  }
+
+  const _class = hover ? 'input-file' : '';
+
+  console.log(data)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='input-wrapper'
+
+      >
+        {/* <label htmlFor='inputFile' className={'label-input ' + _class}>
+          Click here
+        </label> */}
+        <input type='file' value='' onChange={onChange}
+          onDragLeave={() => setHover(false)}
+          onDragEnter={() => setHover(true)}
+          onDrop={() => setHover(false)}
+          placeholder=''
+        />
+      </div>
+      <div className='file-list'>
+        {data.map((item, index) =>
+          <div className='item'>
+            <p> {item.name}</p>
+            <p> {item.type}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
+}
+
+function getImageFile(file) {
+  switch (file) {
+    case file === '':
+
+      break;
+
+    default:
+      break;
+  }
 }
 
 export default App;
---
+++ src/assets/images.js
@@ -0,0 +1,13 @@
+export const IMGES ={
+    filecss: require('../assets/css.png'),
+    download: require('../assets/download.png'),
+    exel: require('../assets/exel.png'),
+    html: require('../assets/html.png'),
+    jpeg: require('../assets/jpeg.png'),
+    js: require('../assets/js.png'),
+    media: require('../assets/media.png'),
+    png: require('../assets/png.png'),
+    pp: require('../assets/pp.png'),
+    upload: require('../assets/upload.png'),
+    word: require('../assets/word.png'),
+}
