ad8c340b9e1ba8b335c3da8cb4943b3da5732f64
manhdung287 <manhdung28797@gmail.com>
2021-10-01 18:39:03 +0700

    add sortable

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Layout from './modules/Layout';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
