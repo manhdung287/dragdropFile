e22334a876527f8bc17631457bc8e2f0258bff99
manhdung287 <manhdung28797@gmail.com>
2021-10-04 21:32:54 +0700

    fix

--- src/modules/Pages/DragItem/index.js
+++ src/modules/Pages/DragItem/index.js
@@ -1,17 +1,18 @@
 import { arrayMoveImmutable } from 'array-move';
-import React, { useState } from 'react';
+import React, { createRef, Fragment, useRef, useState } from 'react';
 
 import {
     SortableContainer,
     SortableElement,
     SortableHandle
 } from "react-sortable-hoc";
-import { exampleData } from '../../ultils/exampleData';
+// import { exampleData } from '../../ultils/exampleData';
 
 
 import './index.scss';
 
 const Handle = SortableHandle(({ tabIndex, images }) => (
+
     <div className='tab-item' tabIndex={tabIndex}>
         <img alt='' src={images} className='img-tab' />
     </div>
@@ -44,7 +45,63 @@ const SortableList = SortableContainer(
 
 
 function DragItem() {
-    
+    const exampleData = [
+        {
+            id: 1,
+            preview:
+                "https://res.cloudinary.com/dport/image/upload/v1633138487/1_yf5wmm.jpg",
+            caption: "test 1",
+            starred: true
+        },
+        {
+            id: 2,
+            preview:
+                "https://res.cloudinary.com/dport/image/upload/v1633138487/2_pdpdiy.jpg",
+            caption: "test 2"
+        },
+        {
+            id: 3,
+            preview:
+                "https://res.cloudinary.com/dport/image/upload/v1633138487/3_o4b7cm.jpg",
+            caption: "test 3"
+        },
+        {
+            id: 4,
+            preview:
+                "https://res.cloudinary.com/dport/image/upload/v1633137376/4_vhwoab.jpg",
+            caption: "test 4"
+        },
+        {
+            id: 5,
+            preview:
+                "https://res.cloudinary.com/dport/image/upload/v1633137377/5_fwdjf1.jpg",
+            caption: "test 5"
+        },
+        {
+            id: 6,
+            preview:
+                "https://res.cloudinary.com/dport/image/upload/v1633137376/6_zlddoi.jpg",
+            caption: "test 6"
+        },
+        {
+            id: 7,
+            preview:
+                "https://res.cloudinary.com/dport/image/upload/v1633137376/7_sehl5b.jpg",
+            caption: "test 6"
+        },
+        {
+            id: 8,
+            preview:
+                "https://res.cloudinary.com/dport/image/upload/v1633137377/8_rkmrjl.jpg",
+            caption: "test 6"
+        },
+        {
+            id: 9,
+            preview:
+                "https://res.cloudinary.com/dport/image/upload/v1633137376/9_givl9x.jpg",
+            caption: "test 6"
+        },
+    ]
     const _data = exampleData.sort(() => Math.random() - 0.5);
 
     const [photos, setPhotos] = useState(_data);
@@ -52,8 +109,9 @@ function DragItem() {
     const onSortEnd = ({ oldIndex, newIndex }) => {
         setPhotos(arrayMoveImmutable(photos, oldIndex, newIndex));
     };
+
     return (
-        <div  className='drag-item-container'>
+        <Fragment >
               <SortableList
                 shouldUseDragHandle={true}
                 useDragHandle
@@ -61,7 +119,9 @@ function DragItem() {
                 items={photos}
                 onSortEnd={onSortEnd}
             />
-        </div>
+        </Fragment>
+          
+        
     );
 }
 
