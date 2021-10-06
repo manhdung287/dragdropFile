f11b7b8bdb5329ad7d9cb44c37296eb91ca87172
manhdung287 <manhdung28797@gmail.com>
2021-10-06 18:37:09 +0700

    add css

--- src/modules/Pages/Home/index.js
+++ src/modules/Pages/Home/index.js
@@ -7,7 +7,13 @@ function Home() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.banner}>
-      <img src={banner} alt='banner'/>
+        <img src={banner} alt='banner' />
+        <div className={styles.contentBanner}>
+
+        </div>
+        <div className=' container'>
+          <p className={styles.text}> Xin chào !</p>
+        </div>
       </div>
       <div className="container">
         Chào mừng đến website
