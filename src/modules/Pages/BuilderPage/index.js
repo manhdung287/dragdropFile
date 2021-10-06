e6e132f69eb621978878afd4d2fa332299c191d4
manhdung287 <manhdung28797@gmail.com>
2021-10-06 18:33:08 +0700

    fix build page

--- src/modules/Pages/BuilderPage/index.js
+++ src/modules/Pages/BuilderPage/index.js
@@ -4,26 +4,34 @@ import styles from './index.module.scss';
 import 'grapesjs/dist/css/grapes.min.css';
 import grapesJsPreset from 'grapesjs-preset-webpage';
 
-function BuilderPage(props) {
-    const [editor,setEditor] = useState(null);
-    useEffect(()=>{
-        const editor = grapesjs.init({
-            container :'#editor',
-            plugins:[grapesJsPreset],
-            pluginsOpts:{
-                grapesJsPreset:{}
-            }
-        });
-        setEditor(editor);
-    },[])
-    console.log(editor);
+function BuilderPage() {
     return (
         <div className={styles.wrapper}>
-            <div id='editor'>
-
-            </div>
+           
+            <Editor />
         </div>
     );
 }
 
+function Editor() {
+    const [editor, setEditor] = useState(null);
+    useEffect(() => {
+        const editor = grapesjs.init({
+            container: '#editor',
+            plugins: [grapesJsPreset],
+            pluginsOpts: {
+                grapesJsPreset: {}
+            },
+
+        });
+        setEditor(editor);
+    }, [])
+    console.log(editor);
+    return (
+        <div className={styles.wrapperEditor}>
+            <div id='editor'>
+            </div>
+        </div>
+    );
+}
 export default BuilderPage;
